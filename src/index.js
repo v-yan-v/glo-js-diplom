a7188311839af5f7f71e55d9dd673b9551812615
ChenTsu <wox.chentsu@gmail.com>
2018-11-04 16:24:14 +0300

    gift popup

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import {mainSlider} from "./js/mainSlider";
 import {orderDesign} from "./js/orderDesign";
 import {orderConsultation} from "./js/orderConsultation";
 import {checkSendForm} from "./js/chekAndSendForm";
+import {gift} from "./js/gift";
 
 document.addEventListener('DOMContentLoaded', ()=>{
   mainSlider();
   orderDesign();
   orderConsultation();
   checkSendForm();
+  gift();
 });
---
+++ src/js/gift.js
@@ -0,0 +1,35 @@
+'use strict';
+
+export function gift() {
+  document.getElementsByClassName('fixed-gift')[0].addEventListener('click', (evt)=>{
+    let popup = document.createElement('div');
+    
+    popup.classList.add('popup-gift');
+    popup.innerHTML = `
+    <div class=popup-dialog>
+      <button class=popup-close>&times;</button>
+      <div class=popup-content>
+				<h2>У нас для вас есть подарок!</h2>
+				<img src="img/gift.png" alt="">
+				<p>Введите промо-код IWANTPOPART и получите скидку 30% на портрет любого размера в стиле pop-art!</p>
+			</div>
+		</div>`;
+    
+    document.body.appendChild(popup);
+    popup.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+    evt.target.remove();
+  
+    function closePopup(event){
+      if (event.target.classList.contains('popup-close') || event.target === popup){
+        popup.removeEventListener('click', closePopup);
+        popup.getElementsByClassName('popup-close')[0].removeEventListener('click', closePopup);
+        document.body.removeChild(popup);
+        document.body.style.overflow = '';
+      }
+    }
+  
+    popup.addEventListener('click', closePopup);
+    popup.getElementsByClassName('popup-close')[0].addEventListener('click', closePopup);
+  });
+}
